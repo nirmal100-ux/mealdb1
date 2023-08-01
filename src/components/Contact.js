cbb9142eb971bde5b5701d40a58873f93024a061
nirmal100-ux <nirmalstha783@gmail.com>
2023-08-01 10:14:13 +0545

    updated in about and contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 const Contact = () => {
   return (
 
-    <div className="gap-5 h-[477px] w-[1210px] bg-brown-300  py-40 text-center msm:grid mmd:grid">
+    <div className="gap-5 h-[477px] w-[100%] bg-brown-300  py-40 text-center msm:grid mmd:grid">
       <h1 className="text-2xl font-bold">Contact</h1>
       <h1>Email: thedatadb@gmail.com</h1>
       <p >Related sites:</p>
